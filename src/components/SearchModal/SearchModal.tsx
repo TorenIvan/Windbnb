e3a7b7a4d864e5217dcad7409084212ed22fe2f6
Vaggelis Tzironis <VTzironis@innovationsprint.eu>
2022-08-13 20:19:55 +0300

    Adding font awesome search icon

--- src/components/SearchModal/SearchModal.tsx
+++ src/components/SearchModal/SearchModal.tsx
@@ -33,15 +33,35 @@ const Search: FC<IProps> = (props): JSX.Element => {
           </div>
           <div className="item-bar" id="search-icon">
             <button>
-              <p>Search</p>
+              <p>&#xf002;&nbsp;&nbsp;&nbsp;Search</p>
             </button>
           </div>
         </div>
-        <div className="search-items">
+
+        <div className="search-bar-open">
+          <div className="item-bar editable" id="location-bar">
+            <div className="item-bar-text">
+              <p className="title"> Location</p>
+              <p className="choice"> Helsinki, Finland</p>
+            </div>
+          </div>
+          <div className="item-bar editable" id="guests-bar">
+            <div className="item-bar-text">
+              <p className="title">Guests</p>
+              <p className="choice">Add guests</p>
+            </div>
+          </div>
+          <div className="item-bar" id="search-icon">
+            <button>
+              <p>&#xf002;&nbsp;&nbsp;&nbsp;Search</p>
+            </button>
+          </div>
+        </div>
+        {/* <div className="search-items">
           <div id="location-items"></div>
           <div id="guests-items"></div>
           <div id="search-items"></div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
