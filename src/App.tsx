3e8266c1188646423b8829e14944742e3df2d010
TorenIvan <Vaggelisshmos@gmail.com>
2022-11-05 19:56:01 +0200

    Modifying fetch using an effect

--- src/App.tsx
+++ src/App.tsx
@@ -4,73 +4,86 @@ import modalStyles from "./components/Modal/Modal.module.scss";
 import InfoView from "./components/InfoView/InfoView";
 import InfoLine from "./components/InfoLine/InfoLine";
 import Modal from "./components/Modal/Modal";
-import { useGuestsInfo, useLocationInfo, useModalVisibility } from "./components/Hooks";
+import {
+	useGuestsInfo,
+	useLocationInfo,
+	useModalVisibility,
+} from "./components/Hooks";
 import { FC } from "react";
 import {
-  ModalVisibility,
-  InitialPlace,
-  InitialAdults,
-  InitialChildren,
+	ModalVisibility,
+	InitialPlace,
+	InitialAdults,
+	InitialChildren,
 } from "./utils/Constants";
 import { Guests } from "./utils/Types";
 import Places from "./components/Places/Places";
 
 const App: FC = (): JSX.Element => {
-  const [location, updateLocation] = useLocationInfo(InitialPlace);
-  const [guests, updateGuests] = useGuestsInfo({
-    adults: InitialAdults,
-    children: InitialChildren,
-  });
-  const [modalSearchType, updateModalSearchType] = useModalVisibility(
-    ModalVisibility.Hidden
-  );
+	const [location, updateLocation] = useLocationInfo(InitialPlace);
+	const [guests, updateGuests] = useGuestsInfo({
+		adults: InitialAdults,
+		children: InitialChildren,
+	});
+	const [modalSearchType, updateModalSearchType] = useModalVisibility(
+		ModalVisibility.Hidden
+	);
 
-  const searchPlaces = (location: string, guests: Guests, updateUserChoice = false) => {
-    if (updateUserChoice === true) {
-      updateLocation(location);
-      updateGuests(guests);
-    }
-    updateModalSearchType(ModalVisibility.Hidden);
-  };
+	const searchPlaces = (
+		location: string,
+		guests: Guests,
+		updateUserChoice = false
+	) => {
+		if (updateUserChoice === true) {
+			updateLocation(location);
+			updateGuests(guests);
+		}
+		updateModalSearchType(ModalVisibility.Hidden);
+	};
 
-  const userChoice = useMemo(
-    () => ({ location: location, guests: guests }),
-    [location, guests]
-  );
-  const totalGuests: number = guests.adults + guests.children;
-  const isModalVisible = modalSearchType !== ModalVisibility.Hidden;
-  return (
-    <div className={`App ${isModalVisible ? "fixedBackground" : ""}`}>
-      <div className={`${modalStyles.overlay} ${isModalVisible ? modalStyles.show : ""}`}>
-        <div
-          className={`${modalStyles.modalContainer} ${
-            isModalVisible ? modalStyles.show : ""
-          }`}
-        >
-          <Modal
-            key={modalSearchType}
-            modalVisibilityType={modalSearchType}
-            userChoice={userChoice}
-            setSearchTerms={searchPlaces}
-          />
-        </div>
-      </div>
-      <div className="MainApp">
-        <header>
-          <InfoView
-            setModalVisibility={updateModalSearchType}
-            location={location}
-            totalGuests={totalGuests}
-          />
-          <InfoLine />
-        </header>
-        <main>
-          <Places userChoice={userChoice} />
-        </main>
-        <footer></footer>
-      </div>
-    </div>
-  );
+	const userChoice = useMemo(
+		() => ({ location: location, guests: guests }),
+		[location, guests]
+	);
+	const totalGuests: number = guests.adults + guests.children;
+	const isModalVisible = modalSearchType !== ModalVisibility.Hidden;
+
+	return (
+		<div className={`App ${isModalVisible ? "fixedBackground" : ""}`}>
+			<div
+				className={`${modalStyles.overlay} ${
+					isModalVisible ? modalStyles.show : ""
+				}`}
+			>
+				<div
+					className={`${modalStyles.modalContainer} ${
+						isModalVisible ? modalStyles.show : ""
+					}`}
+				>
+					<Modal
+						key={modalSearchType}
+						modalVisibilityType={modalSearchType}
+						userChoice={userChoice}
+						setSearchTerms={searchPlaces}
+					/>
+				</div>
+			</div>
+			<div className="MainApp">
+				<header>
+					<InfoView
+						setModalVisibility={updateModalSearchType}
+						location={location}
+						totalGuests={totalGuests}
+					/>
+					<InfoLine />
+				</header>
+				<main>
+					<Places userChoice={userChoice} />
+				</main>
+				<footer></footer>
+			</div>
+		</div>
+	);
 };
 
 export default App;
--- src/components/Places/Places.tsx
+++ src/components/Places/Places.tsx
@@ -1,4 +1,4 @@
-import { memo, useRef, useState } from "react";
+import { memo, useRef, useState, useEffect } from "react";
 import { PlacesType, UserChoice } from "../../utils/Types";
 import ErrorComponent from "../Error/ErrorComponent";
 import LoadingStaysComponent from "../Loading/LoadingStaysComponent";
@@ -7,34 +7,42 @@ import PlacesComponent from "./PlacesComponent";
 import PlacesService from "./PlacesService";
 
 const Places = (props: { userChoice: UserChoice }): JSX.Element => {
-  const [loading, setLoading] = useState<boolean>(false);
-  const userChoice = useRef<UserChoice>(props.userChoice);
-  const stays = useRef<PlacesType>([]);
-  const fetchError = useRef<boolean>(false);
+	const [loading, setLoading] = useState<boolean>(false);
+	const userChoice = useRef<UserChoice>(props.userChoice);
+	const stays = useRef<PlacesType>([]);
+	const fetchError = useRef<boolean>(false);
 
-  if (props.userChoice !== userChoice.current) {
-    setLoading(true);
-    fetchError.current = false;
-    userChoice.current = props.userChoice;
+	useEffect(() => {
+		setLoading(true);
+		fetchError.current = false;
+		userChoice.current = props.userChoice;
 
-    (async () => {
-      try {
-        const placesService = new PlacesService(userChoice.current);
-        const places = await placesService.GetPlaces();
-        stays.current = places;
-        setLoading(false);
-      } catch (error) {
-        fetchError.current = true;
-        setLoading(false);
-      }
-    })();
-  }
+		let ignore = false;
+		(async () => {
+			try {
+				const placesService = new PlacesService(userChoice.current);
+				const places = await placesService.GetPlaces();
+				if (ignore === false) {
+					stays.current = places;
+					setLoading(false);
+				}
+			} catch (error) {
+				console.log("Why Error");
+				fetchError.current = true;
+				setLoading(false);
+			}
+		})();
 
-  if (loading === true) return <LoadingStaysComponent />;
-  if (fetchError.current === true) return <ErrorComponent />;
-  if (stays.current.length === 0) return <NoStaysComponent />;
+		return () => {
+			ignore = true;
+		};
+	}, [props.userChoice]);
 
-  return <PlacesComponent stays={stays.current} />;
+	if (loading === true) return <LoadingStaysComponent />;
+	if (fetchError.current === true) return <ErrorComponent />;
+	if (stays.current.length === 0) return <NoStaysComponent />;
+
+	return <PlacesComponent stays={stays.current} />;
 };
 
 export default memo(Places);
