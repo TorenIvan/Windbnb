f5c190ada22e4ec7679b0147e87d2b9dcc24dbdc
TorenIvan <Vaggelisshmos@gmail.com>
2022-11-06 18:56:56 +0200

    Fixing Prettier tabs

--- src/components/Places/PlacesService.ts
+++ src/components/Places/PlacesService.ts
@@ -1,40 +1,40 @@
 import { PlacesType, PlaceType, UserChoice } from "../../utils/Types";
 
 class PlacesService {
-	private readonly country: string;
-	private readonly city: string;
-	private readonly adults: number;
-	private readonly children: number;
+  private readonly country: string;
+  private readonly city: string;
+  private readonly adults: number;
+  private readonly children: number;
 
-	constructor(userChoice: UserChoice) {
-		this.country = userChoice.location.split(",")[0].trim();
-		this.city = userChoice.location.split(",")[1].trim();
-		this.adults = userChoice.guests.adults;
-		this.children = userChoice.guests.children;
-	}
+  constructor(userChoice: UserChoice) {
+    this.country = userChoice.location.split(",")[0].trim();
+    this.city = userChoice.location.split(",")[1].trim();
+    this.adults = userChoice.guests.adults;
+    this.children = userChoice.guests.children;
+  }
 
-	async GetPlaces(): Promise<PlacesType> {
-		try {
-			const allPlaces = await fetch("http://localhost:3000/src/stays.json", {
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-					Accept: "application/json",
-				},
-			});
-			const allPlacesJSON: PlacesType = await allPlaces.json();
-			const places: PlacesType = allPlacesJSON.filter(
-				(place: PlaceType) =>
-					place.country === this.country &&
-					place.city === this.city &&
-					place.maxGuests === this.adults + this.children
-			);
-			return places;
-		} catch (error) {
-			console.error(error);
-			throw new Error(`${error}`);
-		}
-	}
+  async GetPlaces(): Promise<PlacesType> {
+    try {
+      const allPlaces = await fetch("http://localhost:3000/src/stays.json", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+      const allPlacesJSON: PlacesType = await allPlaces.json();
+      const places: PlacesType = allPlacesJSON.filter(
+        (place: PlaceType) =>
+          place.country === this.country &&
+          place.city === this.city &&
+          place.maxGuests === this.adults + this.children
+      );
+      return places;
+    } catch (error) {
+      console.error(error);
+      throw new Error(`${error}`);
+    }
+  }
 }
 
 export default PlacesService;
