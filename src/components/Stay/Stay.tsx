31194a1c9c98d96ce020474d2f0c3338a693ec13
TorenIvan <Vaggelisshmos@gmail.com>
2022-11-29 17:53:53 +0200

    Fixed overflowing bug on smaller screens at the top. SearchBar vertical gap bug to be fixed as well

--- src/components/Stay/Stay.tsx
+++ src/components/Stay/Stay.tsx
@@ -6,13 +6,13 @@ const Stay = ({ stay }: { stay: PlaceType }): JSX.Element => {
   console.log(stay.title);
   return (
     <div className={stayStyles.StayContainer}>
-      <div id={stayStyles["image-container"]}>
-        {/*<img src={stay.photo} />*/}
-      </div>
+      {/* <div id={stayStyles["image-container"]}>
+        <img src={stay.photo} />
+      </div>*/}
       <div className={stayStyles.StayItem}>
-        <div>eee</div>
-        <div></div>
-        <div></div>
+        <p>eee</p>
+        <p></p>
+        <p></p>
       </div>
       <div className={stayStyles.StayItem}>
         <p>{stay.title}</p>
